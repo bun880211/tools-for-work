8e4a813ba8769272231a2a997020f2425ce52e87
bun880211 <nejimakidori880211@yahoo.co.jp>
2022-06-11 19:57:22 +0900

    master

---
+++ amcmsadm/python/hello.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#import sys
+#import pprint
+#path = "/home/tools-for-work/amcmsadm/ajs"
+#sys.path.insert(0, path)
+#pprint.pprint(sys.path)
+
+import subprocess
+subprocess.run(["/home/tools-for-work/amcmsadm/ajs/getCalendar.sh"], shell=True)
